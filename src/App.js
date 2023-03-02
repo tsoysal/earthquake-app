e7a8bb61134b5671491fb82a3be35ec59e3fe985
Tahaberk SOYSAL <berksysl.g@gmail.com>
2023-03-02 13:25:00 +0300

    Api added and datas displayed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
+import {useState, useEffect} from 'react';
+import EarthQuakeNode from './EarthQuakeNode';
 
 function App() {
+
+  const [list, setList] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.orhanaydogdu.com.tr/deprem/kandilli/live?limit=30")
+      .then((response) => response.json())
+      .then((data) => setList(data.result));
+  }, [])
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+
+      <header>
+        <h1>Türkiye Son Depremler</h1>
+        <h4>Geçmiş olsun Türkiye</h4>
+      </header>
+
+      <h5>En Son : </h5>
+
+      {list.map((node) => {
+        let id = node.earthquake_id;
+        return <EarthQuakeNode key={id} title={node.title} mag={node.mag} depth={node.depth} date_day={node.date_day} date_hour={node.date_hour} />
+      })}
+
+      <footer>
+        <p>
+          Bu web sitesi bilgilendirme amaçlı oluşturulmuş olup herhangi bir
+          ticari amaç barındırmamaktadır. Bu sitedeki deprem raporları{" "}
+          <a href="http://www.koeri.boun.edu.tr/scripts/lst0.asp">
+            Kandilli Rasathanesi ve Deprem Araştırma Enstitüsü
+          </a>{" "}
+          kaynak alınarak{" "}
+          <a href="mailto:berksysl.g@gmail.com">Tahaberk SOYSAL</a> tarafından
+          hazırlanmıştır.
+          <br />
+          <br />
+          "Türk Milletinin Başı Sağolsun"
+        </p>
+      </footer>
+      
     </div>
   );
 }
---
+++ src/EarthQuakeNode.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function EarthQuakeNode({title, mag, depth, date_hour, date_day}) {
+
+    const reverseString = (str) => {
+        let splitString = str.split("-");
+        let reverseArray = splitString.reverse();
+        let joinArray = reverseArray.join("/");
+
+        return joinArray;
+    }
+
+  return (
+    <div className="container">
+      <h2 className="title">{title}</h2>
+      <hr />
+      <div className="infos">
+        <section className="infobox1">
+          <p>
+            Büyüklük: <span className="size">{mag}</span>
+          </p>
+          <p>Derinlik: {depth} km</p>
+        </section>
+        <section className="infobox2">
+          <p>Saat: {date_hour}</p>
+          <p>Tarih: {reverseString(date_day)}</p>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default EarthQuakeNode;
